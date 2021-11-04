30a62930fcfff86169d99790b50ef256fd8ce8df
Eduardo Riol <eriol@ebay.com>
2021-11-04 10:37:11 +0100

    Improving readability of script output

--- test_deployment.sh
+++ test_deployment.sh
@@ -28,26 +28,36 @@ else
     docker-compose -f $1 up -d
 
     # Smoke Test 1: Check that containers are up and running
+    echo ""
+    echo "**************************************************"
+    echo ""
+    echo "---> Starting execution of Test 1: check containers status"
+    echo ""
     # Check Ghost container
     GHOST_CONTAINER_RUNNING=$(docker ps | awk '/_ghost_1/ && /Up/ { print; }' | wc -l)
     if [[ "$GHOST_CONTAINER_RUNNING" -eq 1 ]]
     then
-        echo "Ghost container is running. Test successful."
+        echo "Ghost container is running. TEST SUCCESSFUL."
     else
-        echo "Ghost container is not running. Test failed."
+        echo "Ghost container is not running. TEST FAILED."
         exit 1
     fi
     # Check MariaDB container
     MARIADB_CONTAINER_RUNNING=$(docker ps | awk '/_ghost_1/ && /Up/ { print; }' | wc -l)
     if [[ "$MARIADB_CONTAINER_RUNNING" -eq 1 ]]
     then
-        echo "MariaDB container is running. Test successful."
+        echo "MariaDB container is running. TEST SUCCESSFUL."
     else
-        echo "MariaDB container is not running. Test failed."
+        echo "MariaDB container is not running. TEST FAILED."
         exit 1
     fi
 
     # Smoke Test 2: Apply test queries in database
+    echo ""
+    echo "**************************************************"
+    echo ""
+    echo "---> Starting execution of Test 2: check basic database queries"
+    echo ""
     # The test is implemented in Java 11 in the ./db-smoke-test dir
 
     # Parse db name, user and password from compose file
@@ -63,14 +73,19 @@ else
     mvn -f ./db-smoke-test exec:java -Dexec.mainClass="io.eduriol.MariaDBTest"
     if [[ "$?" -eq 0 ]]
     then
-        echo "Basic operations were performed in MariaDB. Test successful."
+        echo "Basic operations were performed in MariaDB. TEST SUCCESSFUL."
     else
-        echo "Unable to perform basic operations in MariaDB. Test failed."
+        echo "Unable to perform basic operations in MariaDB. TEST FAILED."
         exit 1
     fi
 
 
     # Smoke Test 3: Log in the Ghost application
+    echo ""
+    echo "**************************************************"
+    echo ""
+    echo "---> Starting execution of Test 3: log in the Ghost app"
+    echo ""
     # The test is implemented in Java 11 in the ./ui-smoke-test dir
 
     # Parse user mail, password and port from compose file
@@ -86,11 +101,19 @@ else
     mvn -f ./ui-smoke-test exec:java -Dexec.mainClass="io.eduriol.GhostUITest"
     if [[ "$?" -eq 0 ]]
     then
-        echo "Log in the Ghost UI worked. Test successful."
+        echo "Log in the Ghost UI worked. TEST SUCCESSFUL."
     else
-        echo "Unable to log in the Ghost UI. Test failed."
+        echo "Unable to log in the Ghost UI. TEST FAILED."
         exit 1
     fi
 fi
+
+echo ""
+echo "**************************************************"
+echo ""
+echo "---> ALL THE TESTS WERE EXECUTED SUCCESSFULLY <---"
+echo ""
+echo "**************************************************"
+echo ""
 
 exit 0
